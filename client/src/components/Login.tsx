72ea417f72601b9fc9934baea62e5ac1c38b78ee
unknown <mounahamdi72@gmail.com>
2023-08-07 18:22:33 +0100

    regsiter+login functions

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -11,7 +11,7 @@ export default function Login({ login }: Props) {
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+                <h1 className="text-3xl font-semibold text-center text-green-500 underline">
                     Sign in
                 </h1>
                 <form className="mt-6">
@@ -26,7 +26,7 @@ export default function Login({ login }: Props) {
                             type="email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
-                            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                            className="block w-full px-4 py-2 mt-2 text-green-500 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <div className="mb-2">
@@ -40,12 +40,12 @@ export default function Login({ login }: Props) {
                             type="password"
                             value={password}
                             onChange={e => setPassword(e.target.value)}
-                            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                            className="block w-full px-4 py-2 mt-2 text-green-500 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
                     <a
                         href="#"
-                        className="text-xs text-purple-600 hover:underline"
+                        className="text-xs text-green-500 hover:underline"
                     >
                         Forget Password?
                     </a>
@@ -55,7 +55,7 @@ export default function Login({ login }: Props) {
                                 e.preventDefault();
                                 login(email, password)
                             }}
-                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-green-600 rounded-md hover:bg-green-700 focus:outline-none focus:bg-green-700">
                             Login
                         </button>
                     </div>
@@ -66,7 +66,7 @@ export default function Login({ login }: Props) {
                     Don't have an account?{" "}
                     <a
                         href="#"
-                        className="font-medium text-purple-600 hover:underline"
+                        className="font-medium text-green-500 hover:underline"
                     >
                         Sign up
                     </a>
